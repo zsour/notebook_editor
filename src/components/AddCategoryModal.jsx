320052633c828662787af116f17017c806541d2a
Daniel Karlsson <daniel.karlsson36@outlook.com>
2024-01-19 23:59:01 +0100

    Toggle modal.

--- src/components/AddCategoryModal.jsx
+++ src/components/AddCategoryModal.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-import './style/AddCategoryModal.css';
-
-
+import React from "react";
+import "./style/AddCategoryModal.css";
+import { useEditorMediator } from "./EditorMediator";
 export default function AddCategoryModal() {
+  const em = useEditorMediator();
+
+  
+
   return (
-    <div class="addCategoryModalContainer"></div>
-  )
+    <div
+      class="addCategoryModalContainer"
+      style={{
+        display: em.addCategoryModalOpen ? "block" : "none",
+      }}
+    ></div>
+  );
 }
--- src/components/EditorMediator.jsx
+++ src/components/EditorMediator.jsx
@@ -5,8 +5,19 @@ export const EditorMediatorContext = React.createContext(null);
 export const useEditorMediator = () => useContext(EditorMediatorContext);
 
 export const EditorMediatorProvider = ({ children }) => {
+  const [addCategoryModalOpen, setAddCategoryModalOpen] = useState(false);
+
+  function toggleCategoryModal() {
+    setAddCategoryModalOpen((prev) => !prev);
+  }
+
   return (
-    <EditorMediatorContext.Provider value={{}}>
+    <EditorMediatorContext.Provider
+      value={{
+        addCategoryModalOpen,
+        toggleCategoryModal,
+      }}
+    >
       {children}
     </EditorMediatorContext.Provider>
   );
