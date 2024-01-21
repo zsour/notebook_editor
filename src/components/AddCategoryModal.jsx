8e7ac7837e4081c26a37abd5232591d6b34abd89
z_sour <daniel.karlsson36@outlook.com>
2024-01-21 21:45:34 +0100

    Add category modal.

--- src/components/AddCategoryModal.jsx
+++ src/components/AddCategoryModal.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style/AddCategoryModal.css";
 import { useEditorMediator } from "./EditorMediator";
 export default function AddCategoryModal() {
   const em = useEditorMediator();
 
-  
+  const [label, setLabel] = useState("");
 
   return (
     <div
@@ -17,7 +17,28 @@ export default function AddCategoryModal() {
         em.toggleCategoryModal();
       }}></div>
 
-      <div className="addCategoryFormContainer"></div>
+      <div className="addCategoryFormContainer">
+        <form onSubmit={(e) => {e.preventDefault()}}>
+          <div className="addCategoryFormHeaderContainer">
+            <b className="addCategoryFormHeader">Add new category</b>
+          </div>
+          
+          <div className="addCategoryFormHeaderContainer">
+            <p className="addCategoryFormHeader">Category label:</p>
+          </div>
+          <input type="text" value={label} className="addCategoryFormInput" onChange={(e) => {setLabel(e.target.value)}} />
+          <div className="addCategoryFormButtonContainer">
+            <div className="addCategoryFormButton" onClick={() => {
+              //Add new category.
+              setLabel("");
+              em.toggleCategoryModal();
+            }}>
+
+              <p>Save</p>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
